432e5529118fb15feba0294b5afe8cb830d63855
=mocnykarolek <karolek.oledzki@gmail.com>
2025-12-22 18:34:57 +0100

    dev2

--- web/index.php
+++ web/index.php
@@ -11,7 +11,21 @@ require_once 'business.php';
 require_once 'controller.php';
 require_once 'routing.php';
 
-$action = $_GET['action'] ?? 'home';
+$action = $_SERVER['REQUEST_URI'];
+$action = str_replace('/index.php', '', $action); // Czyścimy śmieci
+$action = strtok($action, '?'); // Odcinamy parametry
+
+// 2. RATUNEK DLA STARYCH LINKÓW:
+// Jeśli adres jest pusty (czyli jesteś na index.php), ale masz stary parametr ?action=...
+// To zamień go ręcznie na nowy format (np. z "gallery" zrób "/gallery")
+if (($action == '' || $action == '/') && isset($_GET['action'])) {
+    $action = '/' . $_GET['action'];
+}
+
+// 3. Domyślna strona główna
+if ($action == '') {
+    $action = '/';
+}
 
 require_once '../views/header.php';
 
--- web/routing.php
+++ web/routing.php
@@ -2,21 +2,21 @@
 
 $routing = [
     '/' => 'home_controller',
-    'home' => 'home_controller',
-    'gallery' => 'gallery_controller',
-    'aboutme' => 'aboutme_controller',
-    'recipes' => 'przepisy_controller',
-    'form' => 'form_controller',
-    'answer' => 'answer_controller',
-    'upload' => 'upload_controller'
+    '/home' => 'home_controller',
+    '/gallery' => 'gallery_controller',
+    '/aboutme' => 'aboutme_controller',
+    '/recipes' => 'przepisy_controller',
+    '/form' => 'form_controller',
+    '/answer' => 'answer_controller',
+    '/upload' => 'upload_controller'
 
 ];
 
 function dispatch($routing, $action_url) {
-    $action = $action_url;
     
-    if (array_key_exists($action, $routing)) {
-        $function_name = $routing[$action];
+    
+    if (array_key_exists($action_url, $routing)) {
+        $function_name = $routing[$action_url];
         $function_name(); 
     } else {
         http_response_code(404);
